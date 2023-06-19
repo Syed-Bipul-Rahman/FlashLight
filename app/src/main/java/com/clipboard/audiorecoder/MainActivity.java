b50106cea5df570a64970fa879fa8bb569650496
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-19 12:49:11 +0600

    Flashligt action added

--- app/src/main/java/com/clipboard/audiorecoder/MainActivity.java
+++ app/src/main/java/com/clipboard/audiorecoder/MainActivity.java
@@ -1,12 +1,17 @@
 package com.clipboard.audiorecoder;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,15 +35,46 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("FLASHLIGHT OFF");
 
         boolean isFlashAavilable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-        if (!isFlashAavilable){
+        if (!isFlashAavilable) {
             flashNotFound();
+        }
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isCheck) {
+                //flash on method call
+                flashOn(isCheck);
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+
+    private void flashOn(boolean isCheck) {
+        try {
+            if (isCheck == true) {
+                textView.setText("FLASHLIGHT ON");
+            } else {
+                textView.setText("FLASHLIGHT OFF");
+            }
+            cameraManager.setTorchMode(cameraId, isCheck);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
 
     private void flashNotFound() {
 
-        AlertDialog alertDialog=new AlertDialog.Builder(this).create();
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
 
         alertDialog.setTitle("Opps");
         alertDialog.setMessage("Flash is not Available in this Device!");
